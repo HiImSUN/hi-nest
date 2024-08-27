106a9275278c33429f5d8a37468b822067c43605
HiImSUN <kshk08@hanyang.ac.kr>
2024-08-28 04:59:36 +0900

    movies controller 제작. Get, Post, Delete, Patch 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 //루트모듈과 같은 역할의 AppModule이 있다.
 //AppModule에서는 우리가 하는 모든 걸 import할 것이다.
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [MoviesController],
+  providers: [],
 })
 export class AppModule {}
