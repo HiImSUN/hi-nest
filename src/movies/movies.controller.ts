e82765d8302c4f74deaf218ca21655b4805b4c41
HiImSUN <kshk08@hanyang.ac.kr>
2024-08-29 02:10:50 +0900

    검색,  Routes 더 추가

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Post, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Body,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -6,25 +15,35 @@ export class MoviesController {
   getAll() {
     return 'This will return all movies';
   }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after: ${searchingYear}`;
+  }
+
   //무언가를 원한다면, 요청할 것.
-  @Get('/:id')
+  @Get(':id') //이 :id는, 다른 요청보다 뒤에 있어야 한다. else느낌. 왜냐하면 search와 같은 키워드 전부를 id라고 생각하기 때문
   getOne(@Param('id') id: string) {
     return `This will return one movie with the id: ${id}`;
   }
 
   @Post()
-  create() {
+  create(@Body() movieData) {
+    // console.log(movieData);
     return 'This will create a movie';
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   remove(@Param('id') movieId: string) {
     return `This will delete a movie with the id: ${movieId}`;
   }
 
-  @Patch('/:id')
-  patch(@Param('id') movieId: string) {
-    return `This will patch a movie with the id: ${movieId}`;
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
 //
